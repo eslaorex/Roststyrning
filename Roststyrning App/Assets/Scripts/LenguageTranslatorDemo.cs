3ca48591e8786def5ffe03e67134f8d601df33da
Eslao rex <eslao_rex@yahoo.com>
2018-04-18 12:04:58 +0200

    log

--- Roststyrning App/Assets/Scripts/LenguageTranslatorDemo.cs
+++ Roststyrning App/Assets/Scripts/LenguageTranslatorDemo.cs
@@ -1,13 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using IBM.Watson.DeveloperCloud.Services.LanguageTranslator.v2;
 
 public class LenguageTranslatorDemo : MonoBehaviour {
 
-    //public Text ResponseTextField;
+    public Text ResponseTextField;
 
-    private LenguageTranslatorDemo lenguageTranslator;
-
+    private LanguageTranslator languageTranslator;
+    private string _translationModel = "en-es";
 
 	// Use this for initialization
 	void Start () {
