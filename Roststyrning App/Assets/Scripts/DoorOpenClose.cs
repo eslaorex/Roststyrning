0db7a06144ad24f1e58d64b8ed08cf0dcf93fe7a
Eslao rex <eslao_rex@yahoo.com>
2018-04-25 11:50:33 +0200

    testScene05

---
+++ Roststyrning App/Assets/Scripts/DoorOpenClose.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpenClose : MonoBehaviour {
+    public Animator _animator;
+
+
+	// Use this for initialization
+	void Start () {
+        _animator = GetComponent<Animator>();
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Debug.Log("COLLLIDER");
+            _animator.SetBool("open", true);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+
+}
