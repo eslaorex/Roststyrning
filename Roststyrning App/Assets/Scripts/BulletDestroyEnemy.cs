311e77e565e4008894c21cab1ec0f2a38cfa322a
Eslao rex <eslao_rex@yahoo.com>
2018-04-25 13:45:17 +0200

    EnemiesAI

---
+++ Roststyrning App/Assets/Scripts/BulletDestroyEnemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletDestroyEnemy : MonoBehaviour {
+    private int points;
+    public Canvas scoreCanvas;
+    private bool isScoreCanvasActive;
+    public GameObject scoreCanvas2;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //scoreCanvas.enabled = true;
+        if (collision.transform.tag == "Enemy")
+        {
+            
+            Destroy(collision.gameObject);
+   
+            //gameObject.SetActive(true);
+            //SumScore.Add(10);
+            
+            
+
+        }
+
+    }
+
+
+
+}
---
+++ Roststyrning App/Assets/Scripts/HealtScript.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class HealtScript : MonoBehaviour {
+
+    public float health;
+
+    public Image currentHealthBar;
+    public Text ratioText;
+
+    private float hitPoint = 150;
+    private float maxHitPoint = 150;
+
+    public int levelToLoad;
+
+    private void Start()
+    {
+        UpdateHealthBar();
+    }
+
+    void Update () {
+		if (health < 0f)
+        {
+            EnemyAIScript.isPlayerAlive = false;
+            //Destroy (gameObject);
+            //DeadPlayer();
+        }
+	}
+
+    private void UpdateHealthBar()
+    {
+        float ratio = hitPoint / maxHitPoint;
+        currentHealthBar.rectTransform.localScale = new Vector3(ratio,1,1);
+        ratioText.text = (ratio * 100).ToString("0") + '%';
+
+
+
+
+    }
+
+    private void TakeDamage(float damage)
+    {
+        hitPoint -= damage;
+        if (hitPoint < 0)
+        {
+            hitPoint = 0;
+            Debug.Log("Dead");
+            DeadPlayer();
+        }
+        UpdateHealthBar();
+
+    }
+
+
+    private void HealDamage(float heal)
+    {
+        hitPoint += heal;
+        if (hitPoint > maxHitPoint)
+        {
+            hitPoint = maxHitPoint;
+            UpdateHealthBar();
+        }
+    }
+
+    void DeadPlayer()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+    
+}
